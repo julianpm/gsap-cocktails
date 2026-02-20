d57f0873d137d91a09ea98e04200dbac7fd53a13
Julian <jmeanchoff@trewknowledge.com>
2026-02-20 15:33:04 -0500

    built about section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
+import About from './components/About'
 
 const App = () => {
 	return (
@@ -14,6 +15,7 @@ const App = () => {
 			<Navbar />
 			<Hero />
 			<Cocktails />
+			<About />
 		</main>
 	)
 }
